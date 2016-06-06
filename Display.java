0079abd9e2f2b21438ea2460586a2a4f1eb7fa0a
Chris Aporta <christopher.aporta@gmail.com>
2016-06-05 20:51:34 -0400

    implement HashMaps

--- Display.java
+++ Display.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 import com.java_data.Tweet;
@@ -42,6 +46,31 @@ public class Display {
     }
     System.out.printf("Hashtags: %s %n", allHashTags);
     System.out.printf("Mentions: %s %n", allMentions);
+
+    Map<String, Integer> mentionCounts = new HashMap<String, Integer>();
+    for (Tweet twt : tweets) {
+      for (String mention : twt.getMentions()) {
+        Integer count = mentionCounts.get(mention);
+        if (count == null) {
+          count = 0;
+        }
+        count++;
+        mentionCounts.put(mention, count);
+      }
+    }
+    System.out.printf("Mention counts: %s %n", mentionCounts);
+
+    Map<String, List<Tweet>> tweetsByAuthor = new HashMap<String, List<Tweet>>();
+    for (Tweet twt : tweets) {
+      List<Tweet> authoredTweets = tweetsByAuthor.get(twt.getAuthor());
+      if (authoredTweets == null) {
+        authoredTweets = new ArrayList<Tweet>();
+        tweetsByAuthor.put(twt.getAuthor(), authoredTweets);
+      }
+      authoredTweets.add(twt);
+    }
+    System.out.printf("Tweets by author: %s %n", tweetsByAuthor);
+    System.out.printf("Tweets by elgringobarbudo: %s %n", tweetsByAuthor.get("elgringobarbudo"));
 
   }
 }
